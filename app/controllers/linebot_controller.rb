fe36c99ea73c748d5ef02ad7acc1b3ae5b1c6ff7
y.okada <ecopack516@gmail.com>
2019-02-11 20:26:10 +0900

    岡田追加

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -15,7 +15,8 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           # 正規表現で「〜』をパターンマッチしてkeywordへ格納
-          keyword = event.message['text'].match(/.*「(.+)」.*/)
+          # keyword = event.message['text'].match(/.*「(.+)」.*/)
+          keyword = event.message['text'].match(/.*"岡田".*/)
           # マッチングしたときのみ入力されたキーワードを使用
           if  keyword.present?
             seed2 = select_word
